29f115bf6f05d832bd0d24af0e7b3dce37f9b764
Nancy Nallely Martinez Dominguez <nancymartinezdominguez11@gmail.com>
2023-06-06 19:39:28 -0600

    Se optimizo el codigo y corrigieron errores

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,33 +1,38 @@
 const analyzer = {  
   getWordCount: (text) => {
-    console.log("este es el texto: " + text);
+    //console.log("este es el texto: " + text);
     // se declara la variable palabrasseparadas y se le asigna el valor del texto que se recibe dividido en palabras
-    var palabrasseparadas = text.split(" ");
+    const palabrasseparadas = text.split(" ");
     // se declara la variable arrsinespacios y por medio de la funcion filter se le eliminan los espacios de la variable palabrasseparadas
-    var arrsinespacios = palabrasseparadas.filter(palabra => palabra !== '');
-    console.log("esto es lo que hace el split: ", text.split(" "));
-    console.log("esto es lo que hace el filter", arrsinespacios);
+    const arrsinespacios = palabrasseparadas.filter(palabra => palabra !== '');
+    //console.log("esto es lo que hace el split: ", text.split(" "));
+    //console.log("esto es lo que hace el filter", arrsinespacios);
     // se declara la variable nunpalabras y se le asigna el tamaño del arreglo arrsinespacios
-    var numpalabras = arrsinespacios.length;
-    console.log("esto es lo que hace el length: ", numpalabras);
+    const numpalabras = arrsinespacios.length;
+    //console.log("esto es lo que hace el length: ", numpalabras);
     // se retorna la variable numero de palabras como resultado 
     return numpalabras;
-        //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    return text;
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    return text;
   },
   getAverageWordLength: (text) => {    
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    return text;
   },
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    return text;
   },
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    return text;
   },
  
     
--- src/index.js
+++ src/index.js
@@ -3,42 +3,35 @@ import analyzer from './analyzer.js';
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
 
 // se crea la contante element para asignarle el valor del elemento que tiene el id reset-buton
-const Element= document.getElementById("reset-button");
+const Element = document.getElementById("reset-button");
 // a la variable elemento se le agrega un eventListener para que se ejecute al hacer click y llame a la funcion que esta entre parentesis
 Element.addEventListener("click", BorrarTexto);
 
 // se crea la constante caja de texto para asignarle el elemento con nombre user-input y que se encuentra en la posision 0
-const cajadetexto= document.getElementsByName("user-input")[0];
+const cajadetexto = document.getElementsByName("user-input")[0];
 // a la variable caja de texto se le agrega un eventlistener que se ejecuta al escribir con el teclado y que llama a la funcion que se encuentra entre parentesis
 cajadetexto.addEventListener("keyup", analizar);
 
 // se declara la variable resultado como global para poder usarla en todas las funciones
-var resultado = ""; 
+let resultado = "";
 
 function BorrarTexto() {
-    // se declara la variable texto y se le asigna el elemento que tenga el nombre user-input
-    let texto = document.getElementsByName("user-input");
-    // al valor que esta en la posicion cero de la variable texto se le asigna vacio para borrar el valor
-    texto[0].value ="";
-    resultado ="";
-    // se llama la funcion analizar para que esta se ejecute y llene los li con el nuevo valor de resultado
-    analizar();
+  // se declara la variable texto y se le asigna el elemento que tenga el nombre user-input
+  const texto = document.getElementsByName("user-input");
+  // al valor que esta en la posicion cero de la variable texto se le asigna vacio para borrar el valor
+  texto[0].value = "";
+  resultado = "";
+  // se llama la funcion analizar para que esta se ejecute y llene los li con el nuevo valor de resultado
+  analizar();
 }
-function analizar(){
-    // saca el valor de todos los elementos que tengan el nombre user-input
-    //saca el valor del elemento que esta en la posicion cero
-    let texto = document.getElementsByName("user-input")[0].value;
-    // llama a la funcion numeroPalabras y le envia el texto
-    numeroPalabras(texto);
-}    
-// funcion que obtiene el numero de palabras de la variable texto que recibe
-function numeroPalabras(texto){
-    // se llama la funcion getWordCount y se le pasa el texto, el valor que retorna se le asigna a nunpalabras
-   resultado = analyzer.getWordCount(texto);
-   // se declara la constante matches y se le asigna todos los elementos que sean un li
-   const matches = document.querySelectorAll("li");
-   // al primer elemento de matches se le asigna el valor palabras mas la variable resultado que contiene el numero de palabras
-   matches[0].innerHTML = "Palabras " + resultado;
+function analizar() {
+  // saca el valor de todos los elementos que tengan el nombre user-input
+  //saca el valor del elemento que esta en la posicion cero
+  resultado = document.getElementsByName("user-input")[0].value;
+  // Selecciona el elemento que tine la clase palabras, y el innerhtml permite asignar un valor al contenido de este elemento
+  // despues del igual se concatena el valor de la palabra palabras con el valor que retorna el metodo getWordCount
+  document.querySelector(".Palabras").innerHTML = "Palabras &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + analyzer.getWordCount(resultado);
 }
+
 
 
