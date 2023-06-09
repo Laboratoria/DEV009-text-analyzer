74ecc0165b292f3318ab970055122e09eb5fe002
Nancy Nallely Martinez Dominguez <nancymartinezdominguez11@gmail.com>
2023-06-09 12:18:28 -0600

    Di funcionalidad a la metrica numeros y di estilo al footer

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,4 +1,4 @@
-const analyzer = {  
+const analyzer = {
   getWordCount: (text) => {
     //console.log("este es el texto: " + text);
     // se declara la variable palabrasseparadas y se le asigna el valor del texto que se recibe dividido en palabras
@@ -20,24 +20,51 @@ const analyzer = {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
   },
   getCharacterCountExcludingSpaces: (text) => {
-    
+    let caracteressinespacios = (text.replace(/\s/g, ''));
+    let sinespacios = caracteressinespacios.length;
+    return sinespacios;
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
-    return text;
   },
-  getAverageWordLength: (text) => {    
-    //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
-    return text;
+  getAverageWordLength: (text) => {
+    const palabrasseparadas = text.split(" ");
+    const arrsinespacios = palabrasseparadas.filter(palabra => palabra !== '');
+    let suma = 0;
+    for (let i = 0; i < arrsinespacios.length; i++) {
+      suma += arrsinespacios[i].length;
+    }
+    let media = suma / arrsinespacios.length;
+    let conDecimal = media.toFixed(2);
+    if (conDecimal > 0) {
+      return conDecimal;
+    }
+    else (conDecimal = 0)
+    return 0;
+
   },
+  //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+
   getNumberCount: (text) => {
-    //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
-    return text;
+    let palabrasseparadas = text.split(" ");
+    let arrsinespacios = palabrasseparadas.filter(palabra => palabra !== '');
+    let contarnumeros = 0;
+    const regex = /^[0-9]*$/;
+    const tienesolonumeros = false;
+    for (let i = 0; i < arrsinespacios.length; i++) {
+      if (regex.test(arrsinespacios[i])) {
+        contarnumeros = contarnumeros + 1
+      }
+    }
+    return contarnumeros;
   },
+  //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+
+
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
-    return text;
+
   },
- 
-    
+
+
 };
 
 export default analyzer;
