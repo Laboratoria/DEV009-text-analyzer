e68ed8b01706cf0a1e27058e413bde2e5d62df90
Bernidpv <bnidpv@gmail.com>
2023-06-12 12:40:02 -0400

    Index js modificado

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 import analyzer from './analyzer.js';
 
-//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+// //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+
+const textarea= document.querySelector("textarea[name='user-input']"); 
+textarea.addEventListener("keyup",function(){ 
+    const contenido = textarea.value;
+    
+    const contarCaracteres = analyzer.getCharacterCount(contenido);
+    document.getElementById("contarCaracteres").innerHTML = contarCaracteres;
+
+    const contarPalabras = analyzer.getWordCount(contenido);
+    document.getElementById("contarPalabras").innerHTML = contarPalabras;
+
+    const sinCaracteres = analyzer.getCharacterCountExcludingSpaces(contenido);
+    document.getElementById("sinCaracteres").innerHTML = sinCaracteres;
+
+    const longitudPromedio = analyzer.getAverageWordLength(contenido);
+    document.getElementById("longitudPromedio").innerHTML = longitudPromedio;
+
+    const contarNumeros = analyzer.getNumberCount(contenido);
+    document.getElementById("contarNumeros").innerHTML = contarNumeros;
+
+    const sumaNumeros = analyzer.getNumberSum(contenido);
+    document.getElementById("sumaNumeros").innerHTML = sumaNumeros;   
+})   
+
+const button= document.getElementById("reset-button"); 
+button.addEventListener("click",()=>{ 
+    const textarea = document.querySelector("textarea[name='user-input']");
+    textarea.value='';
+});
