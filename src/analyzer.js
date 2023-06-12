acd39c2f242359ad13b2a53b4f8a231d9f9faf4e
Bernidpv <bnidpv@gmail.com>
2023-06-12 12:42:55 -0400

    actualizacion con cosas previsas

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,8 +1,13 @@
 const analyzer = {  
+  
   getWordCount: (text) => {
+    const cantidadDePalabras = text.split(" ").length;
+    return cantidadDePalabras;
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
   getCharacterCount: (text) => {
+    const cantidadDeLetras = text.length;
+    return cantidadDeLetras;
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
   },
   getCharacterCountExcludingSpaces: (text) => {
@@ -12,6 +17,9 @@ const analyzer = {
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
   getNumberCount: (text) => {
+    let contenidoSinNumeros = text.replace(/\D/g,'');
+    let numerosEnTotal = contenidoSinNumeros.length;
+    return numerosEnTotal;
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
   },
   getNumberSum: (text) => {
