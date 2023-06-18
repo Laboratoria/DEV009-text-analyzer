5c43cdf905fa60408ea6288b29659bc709508b81
Bernidelpino <b.delpinovillaseca@gmail.com>
2023-06-18 01:30:04 -0400

    orden

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ textarea.addEventListener("keyup", function () {
   const text = textarea.value;
   const contarCaracteres = analyzer.getCharacterCount(text);
   document.getElementById("contarCaracteres").innerHTML = contarCaracteres;
-  const contarPalabras = analyzer.getWordCount(text);
-  document.getElementById("contarPalabras").innerHTML = contarPalabras;
   const sinCaracteres = analyzer.getCharacterCountExcludingSpaces(text);
   document.getElementById("sinCaracteres").innerHTML = sinCaracteres;
+  const contarPalabras = analyzer.getWordCount(text);
+  document.getElementById("contarPalabras").innerHTML = contarPalabras;
   const contarNumeros = analyzer.getNumberCount(text);
   document.getElementById("contarNumeros").innerHTML = contarNumeros;
   const sumaNumeros = analyzer.getNumberSum(text);
@@ -15,15 +15,13 @@ textarea.addEventListener("keyup", function () {
   const longitudPromedio = analyzer.getAverageWordLength(text);
   document.getElementById("longitudPromedio").innerHTML = longitudPromedio;
 })
-
 const button = document.getElementById("reset-button");
 button.addEventListener("click", function () {
   textarea.value = '';
   document.getElementById("contarCaracteres").innerHTML = '';
-  document.getElementById("contarPalabras").innerHTML = '';
   document.getElementById("sinCaracteres").innerHTML = '';
-  document.getElementById("longitudPromedio").innerHTML = '';
+  document.getElementById("contarPalabras").innerHTML = '';
   document.getElementById("contarNumeros").innerHTML = '';
   document.getElementById("sumaNumeros").innerHTML = '';
+  document.getElementById("longitudPromedio").innerHTML = '';
 });
-
